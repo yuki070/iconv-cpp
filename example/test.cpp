52236b37f55e391c0c108684e2b789544345b3af
AsakuraYo <jianqiang070@foxmail.com>
2014-02-25 15:09:51 +0800

    Öع¹´úÂ롣¼ÓÈëFlyWeightÀ࣬ʹCharsetConvertor¿ɹ²ÏíʹÓÃ 	modified:   example/test.cpp 	modified:   include/iconv.hpp

--- example/test.cpp
+++ example/test.cpp
@@ -20,5 +20,19 @@ int main () {
     } catch (...) {
         std::cout << "EXPECT THROW [PASS]" << std::endl;
     }
+
+    // get handler from FlyWeight to reuse the handler;
+    CharsetConvertor *conv1 = FlyWeight::getCharsetConvertor(CharsetConvertor::CHARSET_GBK, CharsetConvertor::CHARSET_UTF8);
+    CharsetConvertor *conv2 = FlyWeight::getCharsetConvertor(CharsetConvertor::CHARSET_UTF8, CharsetConvertor::CHARSET_GBK);
+    std::string str1 = conv1->convert("中华人民共和国");
+    std::string str2 = conv2->convert(str1);
+    if (str2.compare("中华人民共和国") != 0) {
+        std::cerr << "ERROR in line:" << __LINE__ << std::endl;
+        std::cout << "str1 " << str1 << std::endl;
+        std::cout << "str2 " << str2 << std::endl;
+        return 1;
+    } else {
+        std::cout << "EXPECT EQUAL [PASS]" << std::endl;
+    }
     return 0;
 }
--- include/iconv.hpp
+++ include/iconv.hpp
@@ -2,38 +2,40 @@
 #include <errno.h>
 #include <string.h>
 #include <string>
+#include <map>
 #include <stdexcept>
+#include <sstream>
 
 class CharsetConvertor {
 public:
-    CharsetConvertor(const std::string &fromCharset, const std::string &toCharset) {
-        this->fromCharset = fromCharset;
-        this->toCharset = toCharset;
-        this->outputBufferSize = DEFAULT_BUFFER_SIZE;
-        this->outStr = new char[outputBufferSize];
-        conv = iconv_open(toCharset.c_str(), fromCharset.c_str());
-        if (conv == (iconv_t) -1) {
+    CharsetConvertor(const std::string &fromCharset, const std::string &toCharset, size_t outputBufferSize = DEFAULT_BUFFER_SIZE):
+        _sFromCharset(fromCharset),
+        _sToCharset(toCharset),
+        _nOutBufferSize(outputBufferSize) {
+        _szOutStr = new char[_nOutBufferSize];
+        _conv = iconv_open(_sToCharset.c_str(), _sFromCharset.c_str());
+        if (_conv == (iconv_t) -1) {
             throw std::runtime_error(strerror(errno));
         }
     }
 
     virtual ~CharsetConvertor() {
-        if (outStr) {
-            delete []outStr;
+        if (_szOutStr) {
+            delete []_szOutStr;
         }
-        iconv_close(conv);
+        iconv_close(_conv);
     }
 
     void setOutputBufferSize(size_t buffer) {
-        this->outputBufferSize = buffer;
-        if (outStr) {
-            delete []outStr;
+        _nOutBufferSize = buffer;
+        if (_szOutStr) {
+            delete []_szOutStr;
         }
-        this->outStr = new char[outputBufferSize];
+        _szOutStr = new char[_nOutBufferSize];
     }
 
     size_t getOutputBufferSize() const {
-        return this->outputBufferSize;
+        return _nOutBufferSize;
     }
 
     std::string convert(const std::string &input) {
@@ -41,40 +43,58 @@ public:
         char *pIn = const_cast<char *>(inStr);
         size_t inLen = input.length();
 
-        //char *outStr = new char[outputBufferSize];
-        memset(outStr, 0, outputBufferSize);
-        char *pOut = outStr;
-        size_t outLen = outputBufferSize;
+        memset(_szOutStr, 0, _nOutBufferSize);
+        char *pOut = _szOutStr;
+        size_t outLen = _nOutBufferSize;
 
-        size_t ret = iconv(conv, &pIn, &inLen, &pOut, &outLen);
+        size_t ret = iconv(_conv, &pIn, &inLen, &pOut, &outLen);
         if (ret == -1) {
             throw std::runtime_error(strerror(errno));
         }
         std::string output;
 #ifndef _AIX
-        if (toCharset.compare(CHARSET_UNICODE) == 0) {
-            output = std::string(outStr + 2, outStr + (outputBufferSize - outLen));
+        if (_sToCharset.compare(CHARSET_UNICODE) == 0) {
+            output = std::string(_szOutStr + 2, _szOutStr + (_nOutBufferSize - outLen));
         }
 #endif
-        output = std::string(outStr, outStr + (outputBufferSize - outLen));
-        //delete []outStr;
+        output = std::string(_szOutStr, _szOutStr + (_nOutBufferSize - outLen));
         return output;
     }
 
     static const size_t DEFAULT_BUFFER_SIZE;
-    static const char *CHARSET_UNICODE;
-    static const char *CHARSET_GBK;
-    static const char *CHARSET_UTF8;
+    static const char*  CHARSET_UNICODE;
+    static const char*  CHARSET_GBK;
+    static const char*  CHARSET_UTF8;
 private:
-    iconv_t conv;
-    size_t outputBufferSize;
-    std::string fromCharset;
-    std::string toCharset;
-    char *outStr;
+    CharsetConvertor(const CharsetConvertor&);
+    CharsetConvertor &operator =(const CharsetConvertor&);
+
+    iconv_t     _conv;
+    size_t      _nOutBufferSize;
+    std::string _sFromCharset;
+    std::string _sToCharset;
+    char*       _szOutStr;
 };
 
 const size_t CharsetConvertor::DEFAULT_BUFFER_SIZE = 1024;
-const char *CharsetConvertor::CHARSET_UNICODE = "unicode";
-const char *CharsetConvertor::CHARSET_GBK = "GBK";
-const char *CharsetConvertor::CHARSET_UTF8 = "UTF-8";
+const char*  CharsetConvertor::CHARSET_UNICODE = "unicode";
+const char*  CharsetConvertor::CHARSET_GBK = "GBK";
+const char*  CharsetConvertor::CHARSET_UTF8 = "UTF-8";
 
+class FlyWeight {
+public:
+    static CharsetConvertor *getCharsetConvertor(const std::string &fromCharset, const std::string &toCharset);
+private:
+    static std::map<std::string, CharsetConvertor *> convertorMap;
+};
+
+std::map<std::string, CharsetConvertor *> FlyWeight::convertorMap;
+
+CharsetConvertor *FlyWeight::getCharsetConvertor(const std::string &fromCharset, const std::string &toCharset) {
+    std::ostringstream indexStream;
+    indexStream << fromCharset << "_" << toCharset;
+    if (convertorMap.find(indexStream.str()) == convertorMap.end()){
+        convertorMap.insert(std::pair<std::string, CharsetConvertor *>(indexStream.str(), new CharsetConvertor(fromCharset, toCharset)));
+    }
+    return convertorMap[indexStream.str()];
+}
